14f4c2e330fbfc4a58fecb94ae693826fa6d081a
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-12-04 10:51:15 +0700

    contoh stateless component (function component)

---
+++ src/component/StatelessComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './StatelessComponent.css'
+
+const StatelessComponent = () => {
+	return <h1 className="judul">Ini adalah contoh stateless Component</h1>
+}
+
+export default StatelessComponent
