9f294a74cc261484d86a7d85f0c6be174f0c5e79
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-12-04 12:42:37 +0700

    contoh statefull component (class component)

---
+++ src/container/StatefullComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './StatefullComponent.css'
+
+class StatefullComponent extends Component {
+	render() {
+		return <h1 className="title">Ini adalah contoh statefull component</h1>
+	}
+}
+
+export default StatefullComponent
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import StatelessComponent from './component/StatelessComponent'
+import StatefullComponent from './container/StatefullComponent'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<StatelessComponent />, document.getElementById('root'));
+ReactDOM.render(<StatefullComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
